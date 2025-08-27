3db64ca7f19d18dee4f9a08b301e7e6362967563
HarshWfm <163095611+HarshWfm@users.noreply.github.com>
2025-08-27 14:57:30 +0530

    Committed By Harsh Kashyap

---
+++ src/main/java/com/wfm/experts/dashboard/controller/DashboardController.java
@@ -0,0 +1,8 @@
+package com.wfm.experts.dashboard.controller;
+
+import com.wfm.experts.modules.wfm.features.timesheet.entity.Timesheet;
+
+public class DashboardController {
+
+    private Timesheet timesheet;
+}
