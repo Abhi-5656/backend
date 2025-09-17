a57a5543cda4a52a12e686b8a040c9250995df81
HarshWfm <163095611+HarshWfm@users.noreply.github.com>
2025-09-17 11:46:42 +0530

    Committed By Harsh Kashyap

--- src/main/java/com/wfm/experts/setup/wfm/requesttype/entity/RequestType.java
+++ src/main/java/com/wfm/experts/setup/wfm/requesttype/entity/RequestType.java
@@ -29,19 +29,19 @@ public class RequestType {
     @JoinColumn(name = "leave_policy_id")
     private LeavePolicy leavePolicy;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "approval_config_id")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "approval_id")
     private ApprovalConfig approval;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "clubbing_config_id")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "clubbing_id")
     private ClubbingConfig clubbing;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "validation_config_id")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "validation_id")
     private ValidationConfig validation;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "notification_config_id")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "notification_id")
     private NotificationConfig notifications;
 }
